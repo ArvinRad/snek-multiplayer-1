58cbbfaa9ad066614fb66975cd5c6b82848a29e0
ArvinRad <radanarvin@gmail.com>
2021-03-11 05:56:08 +0000

    First Push og play.js

--- play.js
+++ play.js
@@ -9,3 +9,25 @@ const game = new Game(new UserInterface(), new RemoteInterface())
 
 // Begin game
 game.start()
+const net = require('net');
+
+/**
+ * Establishes connection with the game server
+ */
+const connect = function() {
+  const conn = net.createConnection({
+    host: 'localhost',
+    port: 50541
+  });
+  // interpret incoming data as text
+
+  conn.setEncoding('utf8');
+  conn.on("data", () => {
+    console.log("data");
+  });
+
+  return conn;
+}
+
+console.log('Connecting ...');
+connect();
